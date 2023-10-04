5034b36b0eb4b73b8444d9fdd3655956daa507ef
1192859 <wadhwavaishali11@gmail.com>
2023-10-04 12:17:37 +0530

    Fianl's Commit

--- docker-springboot/src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
+++ docker-springboot/src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
@@ -11,7 +11,7 @@ public class DockerSpringbootApplication {
 
 	@GetMapping("/message")
 	public String getMessage(){
-		return "Welcome to Natwest Pvt Groups. ";
+		return "Welcome to Natwest Pvt Limited Groups. ";
 	}
 
 	public static void main(String[] args) {
