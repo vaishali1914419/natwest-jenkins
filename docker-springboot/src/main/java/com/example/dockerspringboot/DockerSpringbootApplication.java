3bdb3c75d25578b994365b88b07f27d301caf8bb
1192859 <wadhwavaishali11@gmail.com>
2023-10-04 12:08:41 +0530

    Second Committ

--- docker-springboot/src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
+++ docker-springboot/src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
@@ -11,7 +11,7 @@ public class DockerSpringbootApplication {
 
 	@GetMapping("/message")
 	public String getMessage(){
-		return "Welcome to Natwest Group. ";
+		return "Welcome to Natwest Groups. ";
 	}
 
 	public static void main(String[] args) {
