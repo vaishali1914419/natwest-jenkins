d490f508dd284133590f7fe19704e179e874c6c9
1192859 <wadhwavaishali11@gmail.com>
2023-10-04 11:11:47 +0530

    Initial Commit

---
+++ docker-springboot/src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
@@ -0,0 +1,21 @@
+package com.example.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerSpringbootApplication {
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Welcome to Natwest. ";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringbootApplication.class, args);
+	}
+
+}
